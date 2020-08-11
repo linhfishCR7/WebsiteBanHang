32d67a0863b2524cc7120fe714b960fbce8fdb07
linhfishCR7 <59117535+linhfishCR7@users.noreply.github.com>
2020-08-11 20:05:21 +0700

    create templates

---
+++ WebsiteBanHang/WebsiteBanHang/Controllers/KhachHangController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteBanHang.Models;
+
+namespace WebsiteBanHang.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        // GET: KhachHang
+        QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+        public ActionResult Index()
+        {
+            // var lstKH = from KH in db.KhachHangs select KH;
+            var lstKH = db.KhachHangs.ToList();
+            return View(lstKH);
+        }
+        public ActionResult Index1()
+        {
+            var lstKH = from KH in db.KhachHangs select KH;
+            return View(lstKH);
+        }
+    }
+}
